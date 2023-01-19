beec9bb37de65c2a50876e251d628416406a2e0c
lizihanl <zli214@my.bcit.ca>
2023-01-18 23:17:50 -0800

    remove unneccessary

--- index.js
+++ index.js
@@ -4,41 +4,11 @@ const app = express();
 app.use(express.json()); // read JSON BODY
 app.use(express.urlencoded({ extended: true }));
 
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
 app.post('/chatbot', (req, res) => {
-    // Handle incoming request 
-});
-
-app.post('/chatbot', (req, res) => {
-    const message = req.body.message;
-    // Process message and get response
-});
-
-app.post('/chatbot', (req, res) => {
-    const message = req.body.message;
-    const number = message.match(/\d+/);
-    // Process message and get response
-});
-
-app.post('/chatbot', (req, res) => {
-    const message = req.body.message;
-    const number = message.match(/\d+/);
-    if (number) {
-        fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
-            res.json({
-                text: data
-            });
-        }).catch(error => {
-            res.json({
-                text: "Sorry, I couldn't find any information about that number."
-            });
-        });
-    }
-});
-
-app.post('/chatbot', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     const message = req.body.message;
     const number = message.match(/\d+/);
     if (number) {
@@ -63,57 +33,3 @@ app.listen(PORT, () => {
     console.log(`Server is running on port 3000`);
 });
 
-
-$(document).ready(function () {
-    $('#chatbot-form').submit(function (event) {
-        event.preventDefault();
-        // Handle form submission  
-    });
-});
-
-$(document).ready(function () {
-    $('#chatbot-form').submit(function (event) {
-        event.preventDefault();
-        const message = $('#chatbot-input').val();
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/chatbot',
-            data: {
-                message: message
-            },
-            success: function (response) { // Handle response       
-            }
-        });
-    });
-});
-
-$(document).ready(function () {
-    $('#chatbot-form').submit(function (event) {
-        event.preventDefault();
-        const message = $('#chatbot-input').val();
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/chatbot',
-            data: {
-                message: message
-            },
-            // success: function(response) {
-            // 	$('#chatbot-response').text(response.text);
-            // }
-            success: function (response) {
-                let newMessage = $('<div>', {
-                    class: 'message'
-                }).text(response.text);
-                let removeButton = $('<button>', {
-                    class: 'remove-button'
-                }).text('Remove');
-                newMessage.append(removeButton);
-                $('#chat-history').append(newMessage);
-            }
-        });
-    });
-});
-
-$(document).on('click', '.remove-button', function () {
-    $(this).parent().remove();
-});
